4791771fab725ceab465bfe3a5fc073db08317bf
John Syrinek <john@johntron.com>
2012-07-12 20:33:03 -0500

    Adds Backbone

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
-/* Author:
+/* Author: John Syrinek
 
 */
+
+$(function() {
+
+    $('.cta').on('click.sample-code', function(e) {
+        var $target = $(e.target);
+        if ( $target.hasClass('frontend') ) {
+
+        } else if ( $target.hasClass('backend') ) {
+
+        }
+    }); 
+
+});
